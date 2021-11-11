02230fcae4b5044d111568102ce1d21afabeaa51
Banzneri <pasi.hanhijarvi@gmail.com>
2021-11-11 12:33:05 +0200

    fix indentation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import AddBookForm from './AddBookForm'
 import PropTypes from 'prop-types'
 
 const Header = ({ handleAddBook }) => (
-    <div className='flex-container' id='header'>
-        <h1>Books</h1>
-        <AddBookForm handleAddBook={handleAddBook}/>
-    </div>
+  <div className='flex-container' id='header'>
+    <h1>Books</h1>
+    <AddBookForm handleAddBook={handleAddBook}/>
+  </div>
 )
 
 Header.propTypes = {
