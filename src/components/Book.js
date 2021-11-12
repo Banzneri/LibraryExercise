280b28993599643c522ad360bf1780edec5ff22c
Banzneri <pasi.hanhijarvi@gmail.com>
2021-11-12 09:24:28 +0200

    change location of book buttons

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types'
 const Book = ({ book, handleRemoveBook }) => {
   return (
     <div className='book'>
-      <div className='bood-info'>
+      <div className='book-info'>
         <p><b>Name</b>: {book.name}</p>
         <p><b>Genre</b>: {book.genre}</p>
         <p><b>Year</b>: {book.release_year}</p>
         <p><b>Language</b>: {book.language}</p>
       </div>
       <div className='book-buttons'>
-        <input className='remove-book' type='button' value='remove' onClick={() => handleRemoveBook(book.id)}/>
-        <input id='edit book'type='button' value='edit' onClick={() => handleRemoveBook(book.id)}/>
+        <input className='edit-book'type='button' value='edit' onClick={() => handleRemoveBook(book.id)}/>
+        <input className='remove-book' type='button' value='X' onClick={() => handleRemoveBook(book.id)}/>
       </div>
     </div>
   )
